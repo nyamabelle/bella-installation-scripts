fde7a307bfedc9cf89d9091805c3ab1cd16c8043
manganjeh <125208774+nyamabelle@users.noreply.github.com>
2023-06-09 09:46:23 -0400

    Update awscli-eksctl.sh

--- aws-eks/awscli-eksctl.sh
+++ aws-eks/awscli-eksctl.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+sudo apt update 
+sudo apt install zip 
+sudo apt install unzip
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+Verify eksctl version : 
+ eksctl  version 
 
+ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
